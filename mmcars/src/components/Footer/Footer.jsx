ea8b99d76905cb10876cd8d1b9e428cd7420a2cc
Patrick Patchelli <ppatchelli@gmail.com>
2022-09-07 17:58:23 -0300

    create footer

--- mmcars/src/components/Footer/Footer.jsx
+++ mmcars/src/components/Footer/Footer.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React from "react";
+
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import "../../styles/footer.css";
+
+const quickLinks = [
+  {
+    path: "/sobre",
+    display: "Sobre",
+  },
+
+  {
+    path: "#",
+    display: "Privacy Policy",
+  },
+
+  {
+    path: "/carros",
+    display: "Lista de Carros",
+  },
+  {
+    path: "/blogs",
+    display: "Blog",
+  },
+
+  {
+    path: "/contato",
+    display: "Contato",
+  },
+];
 
 const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4" md="4" sm="12">
+            <div className="logo footer__logo">
+              <h1>
+                <Link to="/home" className=" d-flex align-items-center gap-2">
+                  <i class="ri-car-line"></i>
+                  <span>
+                  M&Mcar <br /> Serviços
+                  </span>
+                </Link>
+              </h1>
+            </div>
+            <p className="footer__logo-content">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequuntur, distinctio, itaque reiciendis ab cupiditate harum ex
+              quam veniam, omnis expedita animi quibusdam obcaecati mollitia?
+              Delectus et ad illo recusandae temporibus?
+            </p>
+          </Col>
 
-export default Footer
+          <Col lg="2" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Links Rápidos</h5>
+              <ListGroup>
+                {quickLinks.map((item, index) => (
+                  <ListGroupItem key={index} className="p-0 mt-3 quick__link">
+                    <Link to={item.path}>{item.display}</Link>
+                  </ListGroupItem>
+                ))}
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <div className="mb-4">
+              <h5 className="footer__link-title mb-4">Sede Central</h5>
+              <p className="office__info">R. Dr. Alcides Vieira Arcoverde, 1225 - Jardim das Américas, Curitiba - PR, 81520-260</p>
+              <p className="office__info">Telefone: +55 41 -99770-3672</p>
+
+              <p className="office__info">Email: atendimento@mem.com</p>
+
+              <p className="office__info">Horario do Escritório: 08h00 - 17h00</p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="12">
+            <div className="mb-4">
+              <h5 className="footer__link-title">Newsletter</h5>
+              <p className="section__description">Inscreva-se </p>
+              <div className="newsletter">
+                <input type="email" placeholder="Email" />
+                <span>
+                  <i class="ri-send-plane-line"></i>
+                </span>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="12">
+            <div className="footer__bottom">
+              <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
+                <i class="ri-copyright-line"></i>Copyright {year}, Desenvolvido pelos Mochilas
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
